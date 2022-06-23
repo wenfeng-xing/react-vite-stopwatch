786d0458c855ac2ae7585b04d7755e5a5e0479dc
wenfeng xing <wenfeng.xing@weareadaptive.com>
2022-06-23 13:57:14 -0400

    add the html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="container">
+        <section className="wrapper-time-display">
+          <p>00:00.00</p>
+        </section>
+        <section className="wrapper-controls">
+          <button>Laps</button>
+          <button>Start</button>
+        </section>
+        <section className="laps-list"></section>
+      </div>
     </div>
   )
 }
